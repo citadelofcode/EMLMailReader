9e76107d01bbf58ba273d89a6f0aed03b431de64
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2024-03-28 21:55:01 -0700

    Updated README.md to add new logging mode

--- EMLMailReader/__init__.py
+++ EMLMailReader/__init__.py
@@ -4,7 +4,7 @@ from .Mail_Attachment import MailAttachment, MailAttachmentCollection
 from .Content_Disposition import ContentDisposition
 from .Content_Type import ContentType
 from .Custom_Exceptions import InvalidEncodingError, FileMissingError, IncompleteHeaderError, FolderNotAvailableError, InvalidPropertyError
-from .Enumerations import TransferEncoding, EntityType, DispositionType
+from .Enumerations import TransferEncoding, EntityType, DispositionType, LoggingMode
 from .Mail_Address import MailAddress, MailAddressCollection
 from .Processing_Logs import Logger
 from .Text_Encoding import TextEncoding
